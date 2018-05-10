822294861061a5a44105666135c696639243b129
Thiery Louison <thiery.louison@gmail.com>
2018-05-10 23:02:34 +0400

    Add elb in asg

--- main.tf
+++ main.tf
@@ -4,34 +4,35 @@ provider "aws" {
 }
 
 # Create a VPC to launch our instances into
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-}
-
+#resource "aws_vpc" "default" {
+#  cidr_block = "10.0.0.0/16"
+#
+#}
 # Create an internet gateway to give our subnet access to the outside world
-resource "aws_internet_gateway" "default" {
-  vpc_id = "${aws_vpc.default.id}"
-}
+#resource "aws_internet_gateway" "default" {
+#  vpc_id = "${aws_vpc.default.id}"
+#}
 
 # Grant the VPC internet access on its main route table
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.default.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id}"
-}
-
-# Create a subnet to launch our instances into
-resource "aws_subnet" "default" {
-  vpc_id                  = "${aws_vpc.default.id}"
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-}
+#resource "aws_route" "internet_access" {
+#  route_table_id         = "${aws_vpc.default.main_route_table_id}"
+#  destination_cidr_block = "0.0.0.0/0"
+#  gateway_id             = "${aws_internet_gateway.default.id}"
+#}
+#
+## Create a subnet to launch our instances into
+#resource "aws_subnet" "default" {
+#  vpc_id                  = "${aws_vpc.default.id}"
+#  cidr_block              = "10.0.1.0/24"
+#  map_public_ip_on_launch = true
+#}
 
 # A security group for the ELB so it is accessible via the web
 resource "aws_security_group" "elb" {
   name        = "terraform_example_elb"
   description = "Used in the terraform"
-  vpc_id      = "${aws_vpc.default.id}"
+
+  #vpc_id      = "${aws_vpc.default.id}"
 
   # SSH access from anywhere
   ingress {
@@ -40,7 +41,6 @@ resource "aws_security_group" "elb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   # HTTP access from anywhere
   ingress {
     from_port   = 80
@@ -48,7 +48,6 @@ resource "aws_security_group" "elb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   # outbound internet access
   egress {
     from_port   = 0
@@ -100,6 +99,7 @@ resource "aws_autoscaling_group" "web-asg" {
   desired_capacity     = "${var.desired_capacity}"
   force_delete         = true
   launch_configuration = "${aws_launch_configuration.web-lc.name}"
+  load_balancers       = ["${aws_elb.web-elb.name}"]
 
   #vpc_zone_identifier = ["${split(",",var.availability_zones)}"]
   tags {
--- outputs.tf
+++ outputs.tf
@@ -13,23 +13,3 @@ output "asg_name" {
 output "elb_name" {
   value = "${aws_elb.web-elb.dns_name}"
 }
-
-variable "asg_min" {
-  description = "Min numbers of servers in ASG"
-  default     = "1"
-}
-
-variable "asg_max" {
-  description = "Max numbers of servers in ASG"
-  default     = "2"
-}
-
-variable "asg_desired" {
-  description = "Desired numbers of servers in ASG"
-  default     = "1"
-}
-
-variable "availability_zones" {
-  default     = "us-east-1b,us-east-1c,us-east-1d,us-east-1e"
-  description = "List of availability zones, use AWS CLI to find your "
-}
