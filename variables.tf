386ceb93cf0c84684ef769e3bd16f606ca3d8496
thieryl <thiery.louison@gmail.com>
2018-05-10 17:58:12 +0400

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "public_key_path" {
+  description = <<DESCRIPTION
+Path to the SSH public key to be used for authentication.
+Ensure this keypair is added to your local SSH agent so provisioners can
+connect.
+
+Example: ~/.ssh/terraform.pub
+DESCRIPTION
+
+  default = "~/.ssh/terraform.pub"
+}
+
+variable "key_name" {
+  description = "Desired name of AWS key pair"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "eu-west-1"
+}
+
+# Ubuntu Precise 12.04 LTS (x64)
+variable "aws_amis" {
+  default = {
+    eu-west-1 = "ami-9cbe9be5"
+    eu-west-2 = "ami-c12dcda6"
+  }
+}
