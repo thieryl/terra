33f86418fc71bbb26f1dc55a8b2376afdb05dd3b
thiery louison <thiery.louison@redboxdigital.com>
2018-05-11 15:51:47 +0400

    Add AutoScaling Policies

--- main.tf
+++ main.tf
@@ -3,30 +3,6 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-# Create a VPC to launch our instances into
-#resource "aws_vpc" "default" {
-#  cidr_block = "10.0.0.0/16"
-#
-#}
-# Create an internet gateway to give our subnet access to the outside world
-#resource "aws_internet_gateway" "default" {
-#  vpc_id = "${aws_vpc.default.id}"
-#}
-
-# Grant the VPC internet access on its main route table
-#resource "aws_route" "internet_access" {
-#  route_table_id         = "${aws_vpc.default.main_route_table_id}"
-#  destination_cidr_block = "0.0.0.0/0"
-#  gateway_id             = "${aws_internet_gateway.default.id}"
-#}
-#
-## Create a subnet to launch our instances into
-#resource "aws_subnet" "default" {
-#  vpc_id                  = "${aws_vpc.default.id}"
-#  cidr_block              = "10.0.1.0/24"
-#  map_public_ip_on_launch = true
-#}
-
 # A security group for the ELB so it is accessible via the web
 resource "aws_security_group" "elb" {
   name        = "terraform_example_elb"
@@ -109,53 +85,15 @@ resource "aws_autoscaling_group" "web-asg" {
   }
 }
 
+resource "aws_autoscaling_policy" "bat" {
+  name                   = "foobar3-terraform-test"
+  scaling_adjustment     = 4
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = "${aws_autoscaling_group.web-asg.name}"
+}
+
 resource "aws_key_pair" "auth" {
   key_name   = "${var.key_name}"
   public_key = "${file(var.public_key_path)}"
 }
-
-#resource "aws_instance" "web" {
-#  # The connection block tells our provisioner how to
-#  # communicate with the resource (instance)
-#  connection {
-#    # The default username for our AMI
-#    user = "ec2-user"
-#
-#    # The connection will use the local SSH agent for authentication.
-#  }
-#
-#  instance_type = "t2.micro"
-#
-#  tags {
-#    Name = "tlo-web"
-#  }
-#
-#  # Lookup the correct AMI based on the region
-#  # we specified
-#  ami = "${lookup(var.aws_amis, var.aws_region)}"
-#
-#  # The name of our SSH keypair we created above.
-#  key_name = "${aws_key_pair.auth.id}"
-#
-#  # Our Security group to allow HTTP and SSH access
-#  vpc_security_group_ids = ["${aws_security_group.default.id}"]
-#
-#  # We're going to launch into the same subnet as our ELB. In a production
-#  # environment it's more common to have a separate private subnet for
-#  # backend instances.
-#  subnet_id = "${aws_subnet.default.id}"
-#
-#  # We run a remote provisioner on the instance after creating it.
-#  # In this case, we just install nginx and start it. By default,
-#  # this should be on port 80
-#
-#  provisioner "remote-exec" {
-#    inline = [
-#      "sudo yum update -qy",
-#      "sudo yum install nginx -y",
-#      "sudo service nginx start",
-#    ]
-#  }
-#}
-#
-
